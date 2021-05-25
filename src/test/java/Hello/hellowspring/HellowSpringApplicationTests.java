32f4cc1bb5867ca0ba7b4e896503b55508864dab
DESKTOP-RFRODHF\jung <doizz@naver.com>
2021-05-25 21:12:37 +0900

    최초 커밋

---
+++ src/test/java/Hello/hellowspring/HellowSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Hello.hellowspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
