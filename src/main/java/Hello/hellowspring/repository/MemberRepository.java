68020984f8d7d824780cfcba8b10dbebb0558bb0
DESKTOP-RFRODHF\jung <doizz@naver.com>
2021-05-26 22:40:14 +0900

    spring study day-2

---
+++ src/main/java/Hello/hellowspring/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package Hello.hellowspring.repository;
+
+import Hello.hellowspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+
+
+
+}
